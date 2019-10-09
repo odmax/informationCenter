7f79bd5eeb7a2608e316b2c6929198adfec12555
unknown <doniailoura>
2019-10-09 11:51:44 +0200

    don

--- src/app/anonymous-dashbord/anonymous-dashbord.component.ts
+++ src/app/anonymous-dashbord/anonymous-dashbord.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {UserService} from 'src/app/user.service';
 
 @Component({
   selector: 'app-anonymous-dashbord',
@@ -7,9 +8,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnonymousDashbordComponent implements OnInit {
 
-  constructor() { }
 
+  @Input()anonymousData={
+    id: " ", message: " "
+   
+ }
+ @Input()categoryData={
+   id: " ", name: " "
+ }
+  constructor(private userService:UserService) { }
+  categories:any;
+  test=false;
+  categoryName:any;
+  categoryName1:any;
+  Anonymous=[];
+  complaints=[];
+  compliment=[];
+  suggestion=[];
+
+  
   ngOnInit() {
+  
+    this.getAllAnony();  
+    this.takeAnonycomplaints();
+    this.takeAnonyCompliment();
+    this.takeAnonySuggestion();
+       
+      }
+      takeAnogetCatId(e){
+        this.categoryData.id= e.id;
+        this.anonymousData.id = this.categoryData.id;
+        console.log(this.anonymousData.id);
+        console.log(this.anonymousData.message);
+        this.categoryName=e.name;
+        this.test=true;
+        console.log(e);
+      }
+    
+
+  
+      getAllAnony()
+      {
+        return this.userService.getAnonymous().subscribe(response=>
+          this.Anonymous = response);
+      }
+  
+      takeAnonycomplaints()
+      {
+        return this.userService.getAnonymComplaint().subscribe(data=>
+        this.complaints = data);
+      }
+      takeAnonyCompliment()
+      {
+        return this.userService.getAnonymCompliment().subscribe(data=>
+          this.compliment = data);
+      }
+     takeAnonySuggestion()
+     {
+       return this.userService.getAnonymSuggestion().subscribe(data=>
+        this.suggestion = data);
+     }
   }
 
-}
